9f2c394e1ad17ea2b680077522786ac0c914e832
arpitataa <arpitananda267@gmail.com>
2026-05-18 03:00:06 +0530

    checking frontend functionality and backend deployment

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 function Dashboard() {
   const [logs, setLogs] = useState([])
+  let = 
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState("")
   const [search, setSearch] = useState("")
