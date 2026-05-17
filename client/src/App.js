4aa31acf04c040d3d89bd18a816c73415ea0ae5a
arpitataa <arpitananda267@gmail.com>
2026-05-17 19:05:22 +0530

    test cd flow

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 function Dashboard() {
   const [logs, setLogs] = useState([])
+  const = 
   useEffect(() => {
     axios.get("http://13.206.251.179:5000/api/logs")
       .then(res => setLogs(res.data))
